471f0cc7f4bd7d92763dd892f3780a88e2701135
pavel_novikov <pavelsur07@gmail.com>
2025-06-12 07:07:34 +0300

    feat(fixtures): обновлены тестовые данные для ДДС и новых категорий

--- site/src/DataFixtures/CashflowFixtures.php
+++ site/src/DataFixtures/CashflowFixtures.php
@@ -16,19 +16,17 @@ use Ramsey\Uuid\Uuid;
 
 class CashflowFixtures extends Fixture implements DependentFixtureInterface
 {
-
-
     public function load(ObjectManager $manager): void
     {
         $company = $this->getReference(CompanyFixtures::REFERENCE_COMPANY, Company::class);
         $project = $this->getReference(ProjectFixtures::REFERENCE_PROJECT, Project::class);
-        // --- Счет и Касса --- //
 
+        // --- Счета --- //
         $bank = new CashAccount(Uuid::uuid4()->toString());
         $bank->setCompany($company);
         $bank->setName('Банковский счет основной');
         $bank->setType(CashAccount::BANK);
-        $bank->setOpeningBalance(100000);
+        $bank->setOpeningBalance(200000);
         $manager->persist($bank);
 
         $cash = new CashAccount(Uuid::uuid4()->toString());
@@ -39,77 +37,75 @@ class CashflowFixtures extends Fixture implements DependentFixtureInterface
         $manager->persist($cash);
 
         // --- Категории --- //
-        $categories = [];
+        $op = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $op->setName('Операционная деятельность')->setCompany($company)->setSortOrder(1);
+        $manager->persist($op);
 
-        $revenue = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $revenue->setName('Выручка');
-        $revenue->setCompany($company);
-        $revenue->setSortOrder(1);
+        $revenue = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $revenue->setName('Выручка')->setParent($op)->setCompany($company)->setSortOrder(10);
         $manager->persist($revenue);
 
-        $revenueWb = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $revenueWb->setName('Валбериз')->setParent($revenue)->setCompany($company);
-        $revenueWb->setSortOrder(10);
-        $manager->persist($revenueWb);
-
-        $revenueOzon = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $revenueOzon->setName('Озон')->setParent($revenue)->setCompany($company);
-        $revenueOzon->setSortOrder(20);
+        $revenueOzon = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $revenueOzon->setName('Озон')->setParent($revenue)->setCompany($company)->setSortOrder(20);
         $manager->persist($revenueOzon);
 
-        $production = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $production->setName('Производство и себестоимость')->setCompany($company);
-        $production->setSortOrder(2);
-        $manager->persist($production);
+        $revenueWb = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $revenueWb->setName('ВБ')->setParent($revenue)->setCompany($company)->setSortOrder(30);
+        $manager->persist($revenueWb);
 
-        $materials = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $materials->setName('Материалы')->setParent($production)->setCompany($company);
-        $materials->setSortOrder(30);
+        $materials = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $materials->setName('Материалы')->setParent($op)->setCompany($company)->setSortOrder(40);
         $manager->persist($materials);
 
-        $contractors = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $contractors->setName('Подрядчики')->setParent($production)->setCompany($company);
-        $contractors->setSortOrder(40);
+        $contractors = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $contractors->setName('Подрядчики')->setParent($op)->setCompany($company)->setSortOrder(50);
         $manager->persist($contractors);
 
-        $finance = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $finance->setName('Финансовая деятельность')->setCompany($company);
-        $finance->setSortOrder(3);
-        $manager->persist($finance);
+        $fin = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $fin->setName('Финансовая деятельность')->setCompany($company)->setSortOrder(2);
+        $manager->persist($fin);
 
-        $loans = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $loans->setName('Выплата кредитов и займов')->setParent($finance)->setCompany($company);
-        $loans->setSortOrder(50);
-        $manager->persist($loans);
+        $loansIncome = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $loansIncome->setName('Получение кредитов')->setParent($fin)->setCompany($company)->setSortOrder(60);
+        $manager->persist($loansIncome);
 
-        $invest = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $invest->setName('Инвестиционная деятельность')->setCompany($company);
-        $invest->setSortOrder(4);
-        $manager->persist($invest);
+        $loansPayment = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $loansPayment->setName('Выплата кредитов')->setParent($fin)->setCompany($company)->setSortOrder(70);
+        $manager->persist($loansPayment);
 
-        $projectX = new CashflowCategory(Uuid::uuid4()->toString(),$company);
-        $projectX->setName('Новый проект Х')->setParent($invest)->setCompany($company);
-        $projectX->setSortOrder(60);
-        $manager->persist($projectX);
+        $inv = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $inv->setName('Инвестиционная деятельность')->setCompany($company)->setSortOrder(3);
+        $manager->persist($inv);
+
+        $investX = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $investX->setName('Инвестиции в проект Х')->setParent($inv)->setCompany($company)->setSortOrder(80);
+        $manager->persist($investX);
+
+        $dividendsX = new CashflowCategory(Uuid::uuid4()->toString(), $company);
+        $dividendsX->setName('Получение дивидендов проекта от Х')->setParent($inv)->setCompany($company)->setSortOrder(90);
+        $manager->persist($dividendsX);
 
         $manager->flush();
 
-        // --- Транзакции --- //
+// --- Транзакции --- //
         $transactions = [
+            ['2024-06-01', 100000.00, 'income', 'Выручка Озон', $revenueOzon, $bank],
+            ['2024-06-02', 150000.00, 'income', 'Выручка ВБ', $revenueWb, $cash],
+            ['2024-06-03', 20000.00, 'expense', 'Закупка материалов', $materials, $bank],
+            ['2024-06-04', 15000.00, 'expense', 'Оплата подрядчиков', $contractors, $bank],
+            ['2024-06-05', 20000.00, 'income', 'Получен кредит', $loansIncome, $bank],
+            ['2024-06-06', 20000.00, 'expense', 'Погашение кредита', $loansPayment, $cash],
+            ['2024-06-07', 3000.00, 'expense', 'Инвестиции в проект Х', $investX, $bank],
+            ['2024-06-08', 15000.00, 'income', 'Дивиденды по проекту Х', $dividendsX, $bank],
 
-            ['2024-04-01', 10000.00, 'income', 'Поступление с Валбериз', $revenueWb, $bank],
-            ['2024-04-03', 8000.00, 'income', 'Поступление с Озон', $revenueOzon, $cash],
-            ['2024-04-05', 4000.00, 'expense', 'Покупка ткани и фурнитуры', $materials, $bank],
-            ['2024-04-06', 2000.00, 'expense', 'Оплата пошива', $contractors, $bank],
-            ['2024-04-10', 3000.00, 'expense', 'Погашение займа', $loans, $cash],
-            ['2024-04-15', 10000.00, 'expense', 'Инвестиции в новый проект', $projectX, $bank],
-
-            ['2024-05-01', 120000.00, 'income', 'Поступление с Валбериз', $revenueWb, $bank],
-            ['2024-05-03', 85000.00, 'income', 'Поступление с Озон', $revenueOzon, $cash],
-            ['2024-05-05', 40000.00, 'expense', 'Покупка ткани и фурнитуры', $materials, $bank],
-            ['2024-05-06', 25000.00, 'expense', 'Оплата пошива', $contractors, $bank],
-            ['2024-05-10', 30000.00, 'expense', 'Погашение займа', $loans, $cash],
-            ['2024-05-15', 100000.00, 'expense', 'Инвестиции в новый проект', $projectX, $bank],
+            ['2024-05-01', 100000.00, 'income', 'Выручка Озон', $revenueOzon, $bank],
+            ['2024-05-02', 150000.00, 'income', 'Выручка ВБ', $revenueWb, $cash],
+            ['2024-05-03', 20000.00, 'expense', 'Закупка материалов', $materials, $bank],
+            ['2024-05-04', 15000.00, 'expense', 'Оплата подрядчиков', $contractors, $bank],
+            ['2024-05-05', 20000.00, 'income', 'Получен кредит', $loansIncome, $bank],
+            ['2024-05-06', 20000.00, 'expense', 'Погашение кредита', $loansPayment, $cash],
+            ['2024-05-07', 3000.00, 'expense', 'Инвестиции в проект Х', $investX, $bank],
+            ['2024-05-08', 15000.00, 'income', 'Дивиденды по проекту Х', $dividendsX, $bank],
         ];
 
         foreach ($transactions as [$date, $amount, $direction, $comment, $category, $account]) {
@@ -122,7 +118,6 @@ class CashflowFixtures extends Fixture implements DependentFixtureInterface
             $transaction->setCompany($company);
             $transaction->setAccount($account);
             $transaction->setProject($project);
-
             $manager->persist($transaction);
         }
 
@@ -136,4 +131,5 @@ class CashflowFixtures extends Fixture implements DependentFixtureInterface
             ProjectFixtures::class,
         ];
     }
+
 }
